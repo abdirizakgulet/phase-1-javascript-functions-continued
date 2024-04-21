c1e610ff7ebb4858a8d66ac84fe55a3c179d563a
Aday <abdirizakgulet@gmail.com>
2024-04-22 01:43:26 +0300

    done lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // code your solution here
+function saturdayFun(activity='roller-skate'){
+    if(activity==='roller-skate'){
+        return 'This Saturday, I want to roller-skate!';
+    } else{ return 'This Saturday, I want to bathe my dog!';
+}
+// Test the Function with default argument
+console.log(saturdayFun());
+}
+function mondayWork(){
+
+
+
+
+
+if(typeof mondayWork !== 'function'){
+    function mondayWork(task='go to the office'){
+        console.log(`on Monday, I need to ${task}`);
+    }
+
+} else {
+        mondayWork=function(task='go to the office'){
+            console.log(`On Monday, I need too ${task}`);
+        };
+    }
+
+
+
+mondayWork();
+mondayWork('work on the project');
+};
+
+
+
+function wrapAdjective(flair="*"){
+    return function(adjective="special"){
+        return ;'You are ||a dedicated programmer||!'
+    };
+}
+const encouragingPromptFunction=wrapAdjective("!!!")
+console.log(encouragingPromptFunction("a dedicated programmer"))
+
